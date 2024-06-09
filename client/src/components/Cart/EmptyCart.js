f491d8b44ef79b37ff60e5cc415f4430c6f0ab60
shivdutt0411 <127118849+shivdutt0411@users.noreply.github.com>
2024-06-09 20:42:34 +0530

    Update EmptyCart.js

--- client/src/components/Cart/EmptyCart.js
+++ client/src/components/Cart/EmptyCart.js
@@ -1,5 +1,6 @@
 import { Box,Typography,styled } from "@mui/material"; 
-
+import { DataContext } from "../../context/DataProvider";
+import { useContext } from "react";
 const Component = styled(Box)({
     height:"65vh",
     width:"80%",
@@ -12,13 +13,17 @@ const EmptyContainer = styled(Box)({
     paddingTop:"70px"
 })
 export default function EmptyCart(){
+    const {details} = useContext(DataContext)
     const imgurl = 'https://rukminim1.flixcart.com/www/800/800/promos/16/05/2019/d438a32e-765a-4d8b-b4a6-520b560971e8.png?q=90';
     return(
         <Component>
         <EmptyContainer>
             <img src={imgurl} alt="empty" style={{width:"20%"}}/>
-            <Typography>Your Cart is empty</Typography>
-            <Typography>Add items to your cart</Typography>
+            {details?<><Typography>Your Cart is empty</Typography>
+                <Typography>Add items to your cart</Typography></>:
+                <><Typography>Missing your cart?</Typography>
+                <Typography>Login to see your cart</Typography></>}
+
         </EmptyContainer>
 
         </Component>
