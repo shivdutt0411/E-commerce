9768defdcbe39fb71e0bac0cd3c14531cf58c1e6
shivdutt0411 <127118849+shivdutt0411@users.noreply.github.com>
2024-06-11 22:28:20 +0530

    Update DataProvider.jsx

--- client/src/context/DataProvider.jsx
+++ client/src/context/DataProvider.jsx
@@ -2,11 +2,13 @@ import { createContext,useState } from "react";
 
 export const DataContext = createContext(null);
 export default function DataProvider({children}){
-    const[details,setDetails] = useState('');
+    const[details,setDetails] = useState({});
+    const[cartItems, setCartItems] = useState([]);
+
 
     return(
         <DataContext.Provider value={{
-            details,setDetails
+            details,setDetails,cartItems,setCartItems
         }} >
             {children}
         </DataContext.Provider>
