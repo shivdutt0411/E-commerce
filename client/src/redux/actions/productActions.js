37b91b30b5a8be1ad8a840a89769d033bc5fa229
shivdutt0411 <127118849+shivdutt0411@users.noreply.github.com>
2024-06-11 22:23:11 +0530

    Update productActions.js

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -28,20 +28,3 @@ export const getProductDetails = (id)=>async(dispatch)=>{
 }
 
 
-export const addToCart = (id,quantity)=>async(dispatch)=>{
-    try{
-        const {data} = await axios.get(`${url}/productDetails/${id}`);
-        dispatch({type:actionType.ADD_TO_CART,payload:{...data,quantity}});
-
-
-    }
-    catch(error){
-        dispatch({type:actionType.ADD_TO_CART_ERROR,payload:error.message})
-
-    }
-
-}
-
-export const deleteFromCart = (id)=>(dispatch)=>{
-    dispatch({type:actionType.REMOVE_FROM_CART,payload:id})
-}
