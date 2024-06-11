3a5d3cdc61468a1f6f097cd613805d31283bb0ff
shivdutt0411 <127118849+shivdutt0411@users.noreply.github.com>
2024-06-11 22:24:48 +0530

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,11 @@
 import {createStore,combineReducers, applyMiddleware} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import {thunk} from "redux-thunk";
-import {getProductReducer, getProductDetailsReducer,cartReducer} from "./reducers/productReducer"
+import {getProductReducer, getProductDetailsReducer} from "./reducers/productReducer"
 
 const reducer = combineReducers({
     getProducts: getProductReducer,
     getProductDetails: getProductDetailsReducer,
-    cart:cartReducer
 })
 
 const middleWare = [thunk];
