4a18afe430d2b0cfea3e48b1a0429927b4a7e1e3
shivdutt0411 <127118849+shivdutt0411@users.noreply.github.com>
2024-06-11 22:24:14 +0530

    Update productReducer.js

--- client/src/redux/reducers/productReducer.js
+++ client/src/redux/reducers/productReducer.js
@@ -27,25 +27,3 @@ export function getProductDetailsReducer(state={product:{}},action){
     }
 }
 
-export function cartReducer(state={cartItems:[]},action){
-    switch(action.type){
-        case actionType.ADD_TO_CART:
-            const item = action.payload;
-            const exist = state.cartItems.find(product=>product.id===item.id);
-
-            if(exist){
-                return state;
-
-            }
-            else{
-                return {...state,cartItems:[...state.cartItems,item]}
-            }
-        case actionType.REMOVE_FROM_CART:
-            return {...state,cartItems:state.cartItems.filter(product=>product.id!==action.payload)}
-        case actionType.ADD_TO_CART_ERROR:
-            return {error:action.payload}   
-        default:
-            return state
-    }
-
-}
