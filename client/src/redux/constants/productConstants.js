d0db6d3c49e88235daac3f9f1eb62616b2cec330
shivdutt0411 <127118849+shivdutt0411@users.noreply.github.com>
2024-06-11 22:23:37 +0530

    Update productConstants.js

--- client/src/redux/constants/productConstants.js
+++ client/src/redux/constants/productConstants.js
@@ -5,6 +5,3 @@ export const GET_PRODUCT_DETAILS_REQUEST = 'getProductDetailsRequest';
 export const GET_PRODUCT_DETAILS_SUCCESS = 'getProductsDetailsSuccess';
 export const GET_PRODUCT_DETAILS_FAILURE = 'getProductsDetailsFailure';
 
-export const ADD_TO_CART = "addToCart";
-export const ADD_TO_CART_ERROR= "addToCartError";
-export const REMOVE_FROM_CART = "removeFromCart";
